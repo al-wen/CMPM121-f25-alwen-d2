9314219ca067fb7c1910c13a9da1a01e1827a032
duck <al.wenn05@gmail.com>
2025-10-16 22:45:24 -0700

    Simple marker drawing: Added clear button and draw function for the canvas (step 2 complete)
    Co-authored-by: RTalaro <RTalaro@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,46 @@ import "./style.css";
 document.body.innerHTML = `
   <h1>D2🎨</h1>
   <canvas id="canvas"></canvas>
+  <button id="clear">clear</button>
 `;
 
 const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
 canvas.height = 256;
 canvas.width = 256;
 
-document.appendChild(canvas);
+const clear = document.getElementById("clear")! as HTMLButtonElement;
+
+const context = canvas.getContext("2d")!;
+
+const cursor = {
+  active: false,
+  x: 0,
+  y: 0,
+};
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    context.beginPath();
+    context.moveTo(cursor.x, cursor.y);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+clear.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+// reece was here
